ce5951402a25708df90cfb0bcd290bc0c47cd8b5
Roberto Bani <roberto.bani@gmail.com>
2023-12-30 12:52:34 +0100

    finish second part of multi process version

--- README.md
+++ README.md
@@ -5,35 +5,26 @@ Questo programma C permette di gestire due funzionalità:
    Questa tabella viene salvata all'interno di un file denominato output.csv.
 2) prendere in input un file CSV (presumibilmente il file prodotto dall'output della prima funzionalità), un numero intero indicante un numero di parole, e opzionalmente una parola, e ne fornisca in ouput una frase casuale usando la struttura del file csv, formata dal numero di parole fornite in input, ed eventualmente a partire dalla parola successiva a quella fornita anch'essa come input.
 
+Il software fa uso della libreria ***[GNU GSL](https://www.gnu.org/software/gsl/)*** per la generazione di numeri pseudo-casuali.
 
 ## Installazione
-
-
-
 ```bash
-
+make
 ```
 
-## Utilizzo
-
+## Utilizzo Single Process
 ```bash
 # restituisce il file output.csv contenente la tabella delle parole analizzate in input.txt
 $ ./hw2024sp -1 input.txt
 
 # restituisce il file output.txt contenente la frase composta da 11 parole partendo dalla parola successiva a !
 $ ./hw2024sp -2 output.csv 11 !
-
-# 
-$ ./hw2024mp -1 input.txt -2
 ```
+## Utilizzo Multi Process
+```bash
+# restituisce il file output.csv contenente la tabella delle parole analizzate in input.txt
+$ ./hw2024mp -1 input.txt
 
-## Contributing
-
-Pull requests are welcome. For major changes, please open an issue first
-to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
-
-## License
-
-[MIT](https://choosealicense.com/licenses/mit/)
+# restituisce il file output.txt contenente la frase composta da 11 parole partendo dalla parola successiva a !
+$ ./hw2024mp -2 output.csv 11 !
+```
--- multi-process-version/hw2024mp.c
+++ multi-process-version/hw2024mp.c
@@ -47,13 +47,13 @@ int main(int argc, char *argv[]) {
                     }
                     close(pipe1[0]); // Chiude la parte di lettura della pipe nel figlio
                     // Testa della lista concatenata
-                    struct word_element *head = NULL;
+                    struct word_element *list = NULL;
                     // valorizzo il punto con successore la prima parola della frase
-                    add_or_update_element(&head, ".", received_words[0]);
+                    add_or_update_element(&list, ".", received_words[0]);
                     int j = 1;
                     // valorizza la lista concatenata con le parole dell'array
                     for (int i = 0; j < num_words; i++) {
-                        add_or_update_element(&head, received_words[i], received_words[j]);
+                        add_or_update_element(&list, received_words[i], received_words[j]);
                         j++;
                     }
                     // creazione della seconda pipe
@@ -78,15 +78,15 @@ int main(int argc, char *argv[]) {
                         serialized_list[bytes_read] = '\0';  // Assicurati di terminare la stringa con null character
                         // Deserializza la lista concatenata
                         struct word_element *received_list = deserialize_list(serialized_list);
-                        // Ora puoi utilizzare received_list come desideri
-                        // Ora puoi scrivere il file output.csv utilizzando la lista concatenata
+                        // Ora è possibile scrivere il file output.csv utilizzando la lista concatenata
                         write_output(&received_list, "../output.csv" );
-                        // Libera la memoria utilizzata per la deserializzazione
+                        // Liberare la memoria utilizzata per la deserializzazione
+                        free(received_list);
                         exit(EXIT_SUCCESS);
                     } else {
                         // processo figlio 1
                         // Serializza la lista concatenata prima di inviarla attraverso la pipe
-                        char *serialized_list = serialize_list(head);
+                        char *serialized_list = serialize_list(list);
                         close(pipe2[0]); // Chiudo la pipe in lettura
                         // Invia la lista serializzata attraverso la pipe2
                         write(pipe2[1], serialized_list, strlen(serialized_list) + 1);
@@ -98,7 +98,6 @@ int main(int argc, char *argv[]) {
                     close(pipe1[0]); // Chiude la parte di lettura della pipe
                     // legge il file di input e sposta le parole nell'array di char words ripulendo il testo
                     words = read_text_file(optarg);
-                    int j = 1;
                     for (int i = 0; words[i] != NULL; i++) {
                         write(pipe1[1], words[i], strlen(words[i]) + 1); // Invia ogni parola separatamente
                         usleep(10000);
@@ -115,13 +114,12 @@ int main(int argc, char *argv[]) {
                     optind++;
                     // viene convertito la stringa che indica il numero di parole in un intero
                     char *end_pointer;
-                    number_of_words = strtol(argv[optind], &end_pointer, 10);
+                    number_of_words = (int) strtol(argv[optind], &end_pointer, 10);
                     if (argv[optind] == NULL || *end_pointer != '\0') {
                         // gestisce il caso in cui ci sono caratteri non validi nella stringa
                         perror("Error trying to read number of words, characters not valid");
                         exit(EXIT_FAILURE);
                     }
-
                     optind++;
                     // nel caso in cui è presente prende anche la parola dalla quale iniziale le frasi random
                     start_word = argv[optind];
@@ -129,11 +127,48 @@ int main(int argc, char *argv[]) {
                     perror("Option -2 requires at least two argument\n");
                     exit(EXIT_FAILURE);
                 }
-                // inizializza la lista concatenata vuota
-                struct word_element *list = NULL;
-                // viene letto il file di input e viene costruita la lista concatenata delle parole con i rispettivi successori
-                read_csv_and_build_list(input_file, &list);
-                break;
+                // creazione della pipe
+                if (pipe(pipe1) == -1) {
+                    perror("Error trying to create first pipe");
+                    exit(EXIT_FAILURE);
+                }
+                // creazione del primo processo di lettura del file
+                pid_t pid_3 = fork();
+                if (pid_3 == -1) {
+                    perror("Error trying to fork the third process");
+                    exit(EXIT_FAILURE);
+                } else if (pid_3 == 0) {
+                    // processo figlio
+                    char serialized_list[MAX_SERIALIZED_SIZE];
+                    close(pipe1[1]); // chiudere la pipe in scrittura nel processo figlio 2
+                    ssize_t bytes_read = read(pipe1[0], serialized_list, MAX_SERIALIZED_SIZE);
+                    if (bytes_read <= 0) {
+                        // Gestire l'errore o l'assenza di dati
+                        perror("Error reading from pipe2 or empty serialized list\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    serialized_list[bytes_read] = '\0';  // Assicurati di terminare la stringa con null character
+                    // Deserializza la lista concatenata
+                    struct word_element *received_list = deserialize_list(serialized_list);
+                    // Ora è possibile scrivere il file output.csv utilizzando la lista concatenata
+                    write_text_file(received_list, number_of_words, start_word, "../output.txt");
+                    // Liberare la memoria utilizzata per la deserializzazione
+                    free(received_list);
+                    exit(EXIT_SUCCESS);
+                } else {
+                    // processo padre
+                    // inizializza la lista concatenata vuota
+                    struct word_element *list = NULL;
+                    // viene letto il file di input e viene costruita la lista concatenata delle parole con i rispettivi successori
+                    read_csv_and_build_list(input_file, &list);
+                    char *serialized_list = serialize_list(list);
+                    close(pipe1[0]); // Chiudo la pipe in lettura
+                    // Invia la lista serializzata attraverso la pipe2
+                    write(pipe1[1], serialized_list, strlen(serialized_list) + 1);
+                    waitpid(pid_3, NULL, 0); // Attendi il figlio
+                    exit(EXIT_SUCCESS);
+                }
+
 
             default:
                 //nel caso in cui nessun argomento venga specificato in esecuzione del file viene restituito un errore
